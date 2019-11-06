d825c7fd66dd2610116269fd9e16f8b4b291d749
Claudia <clasersal@gmail.com>
2019-11-06 09:11:34 -0600

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 let interval;
-keys = 5;
 let frames = 0;
 const images = {
   donut: 'Images/donut3.png',
@@ -22,22 +21,33 @@ class Board {
   } //fin constructor
   draw() {
     clearInterval();
-    //this.y--; // preguntamos si la primer imagen ya esta fuera del canvas
+    //this.y -= 3; 
     //if (this.y < -canvas.height && frames % 5 === 0) this.y = 0;
-    // dibujamos la imagen normal
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    //dibujamos la otra imagen, despues de la primer imagen, para que ocupe el espacio en blanco, cuando la primer imagen esta fuera
     //ctx.drawImage(this.img, this.x, this.y + canvas.height, this.width, this.height);
-    //interval = setInterval(update, 2000 / 1);
   }
-}
+  drawCircles() {
+    if (ctx) {
+      for (let i = 0; i < 5; i++) {
+        ctx.fillStyle = '#F73D8C';
+        ctx.strokeStyle = "#F73DE9";
+        ctx.arc(150 + 150 * i, 650, 50, 0, 2 * Math.PI);
+        ctx.fill();
+
+      } //end for
+    } //end if
+  } //end drawCircles
+} // fin de la clase board
 
 class Donut {
   constructor() {
     this.x = 100;
     this.y = 50;
+    this.speed = 6;
     this.width = 100;
     this.height = 100;
+    this.score = 0;
+    this.fails = 0;
     this.img = new Image();
     this.img.src = images.donut;
     this.img.onload = () => {
@@ -46,27 +56,99 @@ class Donut {
   }
 
   draw() {
-    this.y += 1;
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+    this.y += this.speed;
+    console.log(this.y);
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    if (this.y === canvas.height) this.y = 0;
-    interval = setInterval(update, 2000 / 3);
+
   }
   isTouching() {
-    return this.y > 600 && this.y < 700;
+    return (this.y > 550 && this.y < 600);
   }
-}
+
+  isFail() {
+    if (this.y > 601) {
+
+      ctx.font = 'bold 40px Serif';
+      ctx.fillStyle = "magenta";
+      ctx.fillText('Fail!!', 110, 760);
+
+      //this.fails += 1;
+    }
+  } //end isFail
+
+
+
+
+
+
+
+} //end class Donut
 
 const board = new Board();
 const donita = new Donut();
+const donita1 = new Donut();
+donita1.x = 250;
+donita1.speed = 10;
+
+const donita2 = new Donut();
+donita2.x = 400;
+donita2.speed = 9;
+const donita3 = new Donut();
+donita3.x = 550;
+donita3.speed = 10;
+const donita4 = new Donut();
+donita4.x = 700;
+donita1.speed = 12;
+
+
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function update() {
-  // frames++;
-  clearCanvas();
-  board.draw();
-  donita.draw();
-  donita.isTouching();
+window.onload = function () {
+  startGame();
+
+  function startGame() {
+    interval = setInterval(update, 1000 / 60);
+  }
+
+  function update() {
+
+    frames++;
+    clearCanvas();
+    board.draw();
+    board.drawCircles();
+    donita.draw();
+    donita.isFail();
+
+
+
+    console.log(donita.score);
+    console.log(donita.totalScore);
+    //console.log(donita1.isTouching());
+    //donita2.draw();
+    //donita3.draw();
+    //donita4.draw();
+
+    //donita.isTouching();
+  } //end update
 }
+
+document.onkeydown = e => {
+  switch (e.keyCode) {
+    case 65:
+      if (donita.isTouching()) {
+        donita.score += 5;
+        clearCanvas();
+        donita.y += 300;
+
+      }
+
+  } //end switch
+
+
+} //End listener onkeypress
